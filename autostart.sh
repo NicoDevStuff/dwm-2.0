09aadb2370290d78a6918759943adcddc7964cb3
NicoDevStuff <NicoG2008@gmx.de>
2023-06-29 01:05:30 +0200

    Riced up and ready to go 🤓🤓🤓🤓

--- autostart.sh
+++ autostart.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-ksnip &
+
+~/.screenlayout/main.sh &
 nitrogen --restore &
+dwmblocks &
+nm-applet &
+volumeicon &
+# for laptop users only!
+# cbatticon
+conky -c ~/.config/conky/dwm/conky.conf &
+udiskie &
+xclip &
+easyeffects &
+solaar &
+sxhkd -c ~/.config/dwm/sxhkdrc &
